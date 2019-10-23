5de5a3c38b405f859931d1565dcdc6cdd0ae55d9
Marion Douet <squalaladraw@gmail.com>
2019-10-23 11:53:45 +0200

    Ajout fonctionnalité searchBar Home

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SearchHome from './components/SearchHome';
+
+function App() {
+  return (
+    <div>
+      <SearchHome />
+    </div>
+  );
+}
+
+export default App;
