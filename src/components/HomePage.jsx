8abcc38c2b67520007cc071d544c34f140f65aeb
Ericka Barbaux <ericka.barbaux@gmail.com>
2019-10-24 09:40:16 +0200

    Modification de App.js en App.jsx, et creation d'un dossier images

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './HomePage.css';
-import Logo3 from './Logo3.png';
+import Logo3 from '../images/Logo3.png';
 
 function HomePage() {
   return (
     <div className="main-home">
       <img src={Logo3} alt="Website logo" className="img-home" />
       <h1 className="typo-title-home">STEAM FINDER</h1>
-      <section className="search-home"></section>
+      <section className="search-home" />
     </div>
   );
 }
