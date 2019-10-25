b4d92920a4d69dd213512415fb4cc6798dd3917f
Ericka Barbaux <ericka.barbaux@gmail.com>
2019-10-25 12:00:23 +0200

    Error en local reglement de conflit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,14 @@
-
 import React from 'react';
 import './App.css';
 import Nav from './components/Nav';
 import HomePage from './components/HomePage';
 import Footer from './components/Footer';
 
-
 function App() {
   return (
     <div className="App">
-      <HomePage/>
-       <Nav />
+      <Nav />
+      <HomePage />
       <Footer />
     </div>
   );
