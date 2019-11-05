06d4e865b9f1d6c7b9bb266b0eee1384c2b7d56c
user <inesborreill@gmail.com>
2019-11-05 16:09:18 +0100

    formulaire avec ajout route & link

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
 import './App.css';
 import Nav from './components/Nav';
+import Footer from './components/Footer';
+import { Switch, Route } from 'react-router-dom';
 import HomePage from './components/HomePage';
 import SearchHome from './components/SearchHome';
-import Footer from './components/Footer';
+import FormContact from './components/FormContact';
 
 function App() {
   return (
     <div className="App">
       <Nav />
-      <HomePage />
-      <SearchHome />
+      <Switch>
+        <Route exact path="/">
+          <HomePage />
+          <SearchHome />
+        </Route>
+        <Route path="/FormContact">
+          <FormContact />
+        </Route>
+      </Switch>
       <Footer />
     </div>
   );
